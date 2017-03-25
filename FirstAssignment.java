7b276abef2c7fff1ab4de3e9f838aa2a68a6b155
Navinbala5 <bnavin28@gmail.com>
2017-03-25 08:51:15 +0530

    Add files via upload

---
+++ FirstAssignment.java
@@ -0,0 +1,21 @@
+
+public class FirstAssignment {
+
+	public static void main(String[] args) {
+		
+
+			
+			 
+				int integer1=39,integer2=83,sum;
+			sum=integer1-(-integer2);
+		System.out.println("the sum of two integer is " +sum);
+				
+				
+				
+			}
+
+		}
+
+	
+
+
